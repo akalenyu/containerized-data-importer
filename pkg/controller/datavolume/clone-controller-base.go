d8f199a13fd7dcd2c3dd4c5aebac7ed25f6c21f7
Alex Kalenyuk <akalenyu@redhat.com>
2023-01-19 12:53:27 +0200

    Refactor isCrossNamespaceClone to get rid of some some source PVC assumptions
    Signed-off-by: Alex Kalenyuk <akalenyu@redhat.com>

--- pkg/controller/datavolume/clone-controller-base.go
+++ pkg/controller/datavolume/clone-controller-base.go
@@ -618,11 +618,15 @@ func (r *CloneReconcilerBase) syncDataVolumeStatusPhaseWithEvent(
 }
 
 func isCrossNamespaceClone(dv *cdiv1.DataVolume) bool {
-	if dv.Spec.Source.PVC == nil {
-		return false
+	if dv.Spec.Source.PVC != nil {
+		return dv.Spec.Source.PVC.Namespace != "" && dv.Spec.Source.PVC.Namespace != dv.Namespace
 	}
 
-	return dv.Spec.Source.PVC.Namespace != "" && dv.Spec.Source.PVC.Namespace != dv.Namespace
+	if dv.Spec.Source.Snapshot != nil {
+		return dv.Spec.Source.Snapshot.Namespace != "" && dv.Spec.Source.Snapshot.Namespace != dv.Namespace
+	}
+
+	return false
 }
 
 func getTransferName(dv *cdiv1.DataVolume) string {
